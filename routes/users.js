df5384cc64788150d6a7f58758d77b913ff6d6b2
thebigr <nevoroy@gmail.com>
2018-09-30 17:15:55 +0300

    authorization broken.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 //register
 router.get('/register',function (req,res) {
     res.render('register');
@@ -44,5 +46,45 @@ router.post('/register',function (req,res) {
         res.redirect('/users/login');
     }
 });
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.getUserByUsername(username, function (err, user) {
+            if(err) throw err;
+            if(!user){
+                return done(null, false, {message: 'Unknown User'});
+            }
+        User.comparePassword(password, user.password, function (err, isMatch) {
+            if(err) throw err;
+            if(isMatch){
+                return done(null)
+            } else {
+                return done(null, false, {message: 'Invalid password'});
+            }
+        });
+        });
+    }));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.getUserById(id, function (err, user) {
+        done(err,user);
+    });
+});
+
+router.post('/login',
+    passport.authenticate('local',{successRedirect:'/', failureRedirect: '/users/login', failureFlash: true },function(req, res) {
+        res.redirect('/');
+    }))
+
+
+router.get('/logout', function (req,res) {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login')
+})
 
 module.exports = router;
